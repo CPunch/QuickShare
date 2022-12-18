645a73a52d06836b4eea0ef6f41737269e1dc329
CPunch <sethtstubbs@gmail.com>
2022-12-18 01:29:03 -0600

    started api/web

---
+++ api/web/web.go
@@ -0,0 +1,78 @@
+package web
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"strings"
+
+	"github.com/CPunch/QuickShare/api/iface"
+	"github.com/CPunch/QuickShare/config"
+)
+
+type WebClient struct {
+	baseUrl string
+	token   string
+}
+
+func NewClient(baseUrl, token string) *WebClient {
+	return &WebClient{
+		baseUrl: baseUrl,
+		token:   token,
+	}
+}
+
+func (client *WebClient) PostFile(reader io.Reader, filename string) (*iface.File, error) {
+	httpClient := &http.Client{}
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	defer writer.Close()
+
+	// write token
+	tknw, err := writer.CreateFormField("token")
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := io.Copy(tknw, strings.NewReader(client.token)); err != nil {
+		return nil, err
+	}
+
+	// write file
+	fw, err := writer.CreateFormFile("file", filename)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := io.Copy(fw, reader); err != nil {
+		return nil, err
+	}
+
+	// send request
+	req, err := http.NewRequest("POST", client.baseUrl+config.UPLOAD_ENPOINT, bytes.NewReader(body.Bytes()))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Server responded with status code %d!", res.StatusCode)
+	}
+
+	// read response
+	var file iface.File
+	dec := json.NewDecoder(res.Body)
+	if err := dec.Decode(&file); err != nil {
+		return nil, err
+	}
+
+	return &file, nil
+}
--- go.mod
+++ go.mod
@@ -3,12 +3,13 @@ module github.com/CPunch/QuickShare
 go 1.19
 
 require (
-	github.com/blockloop/scan v1.3.0 // indirect
-	github.com/gabriel-vasile/mimetype v1.4.1 // indirect
-	github.com/go-chi/chi/v5 v5.0.8 // indirect
-	github.com/google/subcommands v1.2.0 // indirect
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/mattn/go-sqlite3 v1.14.16 // indirect
-	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
-	gopkg.in/ini.v1 v1.67.0 // indirect
+	github.com/blockloop/scan v1.3.0
+	github.com/gabriel-vasile/mimetype v1.4.1
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/google/subcommands v1.2.0
+	github.com/google/uuid v1.3.0
+	github.com/mattn/go-sqlite3 v1.14.16
+	gopkg.in/ini.v1 v1.67.0
 )
+
+require golang.org/x/net v0.4.0 // indirect
